0b3141c8464d2205b05903f8c1157431dde99fb0
Ailton Duarte <adidas.coyas@gmail.com>
2019-11-25 14:22:24 -0100

    criar link para o site no navbar backend

--- app/Middleware/Authendicated.js
+++ app/Middleware/Authendicated.js
@@ -3,16 +3,18 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Env = use('Env')
+
 class Authendicated {
   /**
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Function} next
    */
-  async handle ({ request, auth, response }, next) {
+  async handle ({ request, auth, response}, next) {
     try {
       await auth.check()
-
+      
       return response.redirect('/')
     } catch (error) {
       // call next to advance the request
