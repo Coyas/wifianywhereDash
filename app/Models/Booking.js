43e3b36951617ac3eff4e4d683013af6a73a923d
Ulisses Cruz <ulissescruz@gmail.com>
2020-02-06 12:53:29 -0100

    [Relação]: Booking - Devices

--- app/Models/Booking.js
+++ app/Models/Booking.js
@@ -1,34 +1,36 @@
-'use strict'
+'use strict';
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use('Model');
 const uuidv4 = require('uuid/v4');
-const Database = use('Database')
+const Database = use('Database');
 
 class Booking extends Model {
   static boot() {
-    super.boot()
+    super.boot();
 
     /**
      * A hook to create booking uuid before saving
      * it to the database.
      */
     // this.addHook('beforeSave', async (userInstance) => { //esta 1 bug aqui, ele faz hash 2x
-    this.addHook('beforeCreate', (payInstance) => {
-      console.log('criando o uuid')
-      payInstance.id = uuidv4.v4()
-
-    })
+    this.addHook('beforeCreate', payInstance => {
+      console.log('criando o uuid');
+      payInstance.id = uuidv4.v4();
+    });
   }
 
   plans() {
-    return this.belongsTo('App/Models/Plan')
+    return this.belongsTo('App/Models/Plan');
+  }
+
+  devices() {
+    return this.belongsTo('App/Models/Device');
   }
 
   static get incrementing() {
-    return false
+    return false;
   }
-
 }
 
-module.exports = Booking
+module.exports = Booking;
